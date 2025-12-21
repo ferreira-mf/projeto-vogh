47d835688076e26f6c4c8ac8ed11f8e25a6f3942
Marcelo <marcelo.ferreira9@live.com>
2025-12-21 12:57:55 -0300

    fix: adiciona pequeno ajuste no FiltroColaboradores.php

--- app/Livewire/FiltroColaboradores.php
+++ app/Livewire/FiltroColaboradores.php
@@ -26,8 +26,7 @@ class FiltroColaboradores extends Component
             ? Unidade::where('bandeira_id', $this->bandeira_id)->orderBy('nome_fantasia')->get()
             : collect();
 
-
-        $colaboradores = ($this->grupo_id && $this->bandeira_id && $this->unidade_id)
+        $colaboradores = $this->unidade_id
             ? Colaborador::where('unidade_id', (int) $this->unidade_id)->orderBy('nome')->get()
             : collect();
 
